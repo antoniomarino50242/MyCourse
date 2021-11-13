e03d30a55d09289c97cd281249b5223350b31162
Antonio Marino <Antonio_marino50242@icloud.com>
2021-11-13 12:35:53 +0100

    Aggiunta cattura eccezioni non controllate

--- Startup.cs
+++ Startup.cs
@@ -63,6 +63,8 @@ namespace MyCourse
                     string filePath = Path.Combine(env.ContentRootPath, "bin/reload.txt");
                     File.WriteAllText(filePath, DateTime.Now.ToString());
                 });
+            } else{
+                app.UseExceptionHandler("/Error");
             }
             app.UseStaticFiles();
             
