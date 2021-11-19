4295937e4228d96fa19f78dc41fcc282e7216658
Antonio Marino <antonio5668@icloud.com>
2021-11-19 17:24:00 +0100

    Commit dopo ripristino mac

---
+++ Customization/TagHelpers/RatingTagHelper.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MyCourse.Customization.TagHelpers
+{
+    public class RatingTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            
+        }
+    }
+}
